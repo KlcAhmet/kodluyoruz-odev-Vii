b6b82a9b3f5ce7a291e9aa6008052db2c8a7265a
Ahmet Batuhan Kılıç <han_batur@hotmail.com>
2021-01-19 05:14:32 +0300

    add button event listener

--- js/style.js
+++ js/style.js
@@ -2,6 +2,10 @@ var named = document.getElementById("name");
 var lastName = document.getElementById("lastName");
 var userName = document.getElementById("userName");
 var mail = document.getElementById("mail");
+var pass1 = document.getElementById("pass1");
+var pass2 = document.getElementById("pass2");
+var btn = document.getElementById("buttoncheck");
+var cardBody = document.querySelector("card__body");
 
 
 
@@ -22,5 +26,12 @@ mail.addEventListener("focusout", function() {
         alert("Geçerli bir mail giriniz.");
     } else {
         mail.classList.remove("input--red");
+    }
+});
+
+
+btn.addEventListener("click", function() {
+    if (pass1.value.length < 8 || pass2.value.length < 8 || pass1.value.length != pass2.value.length) {
+        console.log("aa");
     }
 });
